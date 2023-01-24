f0ac6ca549655e890ae2434dd644b15d674b8f09
Sarthak Sandeep Patil <100191628+SarthakPatil99@users.noreply.github.com>
2023-01-24 20:13:24 +0530

    History module

--- Prakruti_App/models.py
+++ Prakruti_App/models.py
@@ -10,3 +10,34 @@ class Users(models.Model):
     Email = models.EmailField(max_length=40)
     Phone_No = models.IntegerField()
     Admin = models.BooleanField(default=False)
+
+
+class Appointments(models.Model):
+    Day = models.DateField()
+    TimeSlot = models.CharField()
+    Status = models.CharField()
+    P_med = models.CharField()
+
+
+class M_remedy(models.Model):
+    Name = models.CharField()
+    Desc = models.CharField()
+    Content = models.CharField()
+    Quantity = models.IntegerField()
+    Price = models.IntegerField()
+    Img = models.ImageField()
+
+
+class H_remedy(models.Model):
+    Name = models.CharField()
+    Desc = models.CharField()
+    Accessories = models.CharField()
+    Img = models.ImageField()
+
+
+class Blogs(models.Model):
+    Title = models.CharField()
+    Type = models.CharField()
+    Content = models.CharField()
+    text = models.CharField()
+    Img = models.ImageField()
